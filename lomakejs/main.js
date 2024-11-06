9571154ef5033f2cedf1d1eaebbfde5f77020aae
Jarttu <167183980+Jarttu@users.noreply.github.com>
2024-11-06 13:46:29 +0200

    Add files via upload

---
+++ lomakejs/main.js
@@ -0,0 +1,8 @@
+const kayttajaid = document.getElementById('kayttaja').Value;
+const salasana = document.getElementById('salis').Value;
+const nimi = document.getElementById('nimi').Value;
+const osoite = document.getElementById('osote').Value;
+const postinumero = document.getElementById('pnumero').Value;
+const gmail = document.getElementById('sposti').Value;
+
+ 
