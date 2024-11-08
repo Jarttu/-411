70e57a5a211573e7b520a2846ae8b7232c024df5
Jarttu <167183980+Jarttu@users.noreply.github.com>
2024-11-08 03:51:15 +0200

    Update main.js

--- lomakejs/main.js
+++ lomakejs/main.js
@@ -1,8 +1,8 @@
-const kayttajaid = document.getElementById('kayttaja').Value;
-const salasana = document.getElementById('salis').Value;
-const nimi = document.getElementById('nimi').Value;
-const osoite = document.getElementById('osote').Value;
-const postinumero = document.getElementById('pnumero').Value;
-const gmail = document.getElementById('sposti').Value;
+const kayttajaid = document.getElementById('kayttaja').value;
+const salasana = document.getElementById('salis').value;
+const nimi = document.getElementById('nimi').value;
+const osoite = document.getElementById('osote').value;
+const postinumero = document.getElementById('pnumero').value;
+const gmail = document.getElementById('sposti').value;
 
  
