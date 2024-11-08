c898b3de20553853c4036e523926178d558bcebe
Jarttu <167183980+Jarttu@users.noreply.github.com>
2024-11-08 04:21:00 +0200

    Update main.js

--- lomakejs/main.js
+++ lomakejs/main.js
@@ -1,3 +1,5 @@
+document.getElementById('tarkista').addEventListener('click', function(event) {
+event.preventDefault()
 const kayttajaid = document.getElementById('kayttaja').value;
 const salasana = document.getElementById('salis').value;
 const nimi = document.getElementById('nimi').value;
@@ -5,4 +7,11 @@ const osoite = document.getElementById('osote').value;
 const postinumero = document.getElementById('pnumero').value;
 const gmail = document.getElementById('sposti').value;
 
- 
+let virheet = []
+
+if (kayttajaid.length < 6){
+ virheet.push('Virhe: Käyttäjäid ei ole 6 kirjainta pitkä')
+}
+
+if ()
+}
